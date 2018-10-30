3ae469dcc6671eaff4bcfad7bf0ab984e2f9c2fa
Brendan Sapience <brendan.sapience@AAI.AASPL-BRD.COM>
2018-10-30 10:06:14 -0400

    initial release

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PDfSplitLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
+            PdfUtils utils = new PdfUtils();
+            utils.SetFile("MyPdfFile");
+            utils.TestTesseract();
+
         }
     }
 }
